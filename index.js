a273a0b9141961111f5e0e51c8c9a406afb9dbcb
akashswfu <splashakash99@gmail.com>
2024-06-11 02:55:03 +0600

    bugs fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const corsOptions = {
-    origin:['http://localhost:5173'],
+    origin:['http://localhost:5173','https://asset-management-system-auth.web.app'],
     credentials:true,
     optionSuccessStatus:200,
 }
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
 
@@ -593,7 +593,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
